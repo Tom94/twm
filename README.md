3645af6a640146631135175ececa4094bd4c79ec
Thomas Müller <thomas94@gmx.net>
2023-10-23 08:17:07 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# twm: Tom's / tiny / tiling window manager for Windows (with actual BSP!)
+
---
+++ src/main.cpp
@@ -0,0 +1,3 @@
+// This file was developed by Thomas Müller <thomas94@gmx.net>.
+// It is published under the GPU GPLv3 license; see LICENSE.txt for details.
+
